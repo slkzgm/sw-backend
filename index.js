7e3e18915f28ad1d59fc9a67b867a273fe62d496
slkzgm <contact@slkzgm.com>
2024-08-04 18:50:03 +0200

    Removing cors, increasing limit

--- index.js
+++ index.js
@@ -6,24 +6,10 @@ import { simulateMax } from './runeUtils.js';
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
 
-const allowedOrigins = [
-    'https://sw-rune-analyzer.vercel.app'
-];
+app.use(cors());
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-}));
-
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 const validateJson = (req, res, next) => {
     const rawData = req.file.buffer.toString('utf-8');
